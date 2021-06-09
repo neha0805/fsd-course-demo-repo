b4d3608eca70e9a346ef5e7f06c8b11a859ae015
neha.ambr1 <neha.ambre@cumminscollege.in>
2021-06-09 18:16:52 +0530

    second commit

---
+++ Eclipse_java_assignments/src/ass/Ass_1.java
@@ -0,0 +1,25 @@
+package ass;
+
+import java.util.Scanner;
+public class Ass_1
+{
+    public static void main(String[] args)
+    {
+         Scanner sc=new Scanner(System.in);
+         char ch;
+         int asc;
+         System.out.print("Enter a charachter:");
+         ch=sc.next().charAt(0);
+         asc=(int)ch;
+         System.out.print("Ascii value of "+ch+" is: "+asc);
+    }
+}
+
+/*
+Output:
+Enter a charachter:a
+Ascii value of a is: 97
+
+Enter a charachter:A
+Ascii value of A is: 65
+*/
